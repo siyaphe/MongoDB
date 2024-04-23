eeb4f366c3d2df5595b4383e2208faeec8ff92a6
siyaphe <siyapdje@yahoo.fr>
2024-04-23 16:37:59 -0400

    MongoDB first commit

---
+++ server.js
@@ -0,0 +1,42 @@
+require("dotenv").config();
+// -----> allows .env
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 3000;
+const connectToDb = require("./config/connectToDb.js");
+// This pulls our Mongoose connection into application
+
+const Note = require("./models/note");
+const notesController = require("./controllers/notesController.js");
+const cors = require("cors");
+// ---> Recieving reqs on cross-origins **
+app.use(express.json());
+// Express doesnt naturally convert our data to json
+app.use(cors());
+connectToDb();
+// This initializes our connectToDB() function
+// -------------------------------------------------reQs
+
+
+app.get("/", (req, res) => {
+  res.send("This is a Landing Page");
+});
+
+// Obj: We want to establish CRUD routes for our Notes Model
+app.get("/notes", notesController.fetchAllNotes);
+// -----------------> GET all Notes - [Read]
+app.get("/notes/:id", notesController.fetchNote);
+// -----------------> GET a Specific Note by ID - [Read]
+app.post("/notes", notesController.createNote);
+// -----------------> Create a Notes - [Create / POST]
+app.put("/notes/:id", notesController.updateNote);
+// -----------------> Update a Specific Note - [Update]
+app.delete("/notes/:id", notesController.deleteNote);
+// -----------------> Delete a Specific Note - [Delete]
+// -------------------------------------------------Routing
+
+
+app.listen(PORT, () => {
+  console.log(`Express Server Listening on port num: ${PORT}`);
+});
+// -------------------------------------------------Server
